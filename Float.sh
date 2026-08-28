b5982daeccaba10105e9a30951e1d1742d910c5d
Pavansai17 <ankalapavansaiteja@gmail.com>
2026-08-28 11:33:42 +0000

    Float addition of two values with basic calcualtor

---
+++ Float.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#To add two Float values
+a=3.14
+b=7.624
+c=$(echo "$a+$b" | bc) #| means It takes output of first side and takes to next side and bc means - basic calculator[Linux command calculator used to handle floats]
+echo $c
+
